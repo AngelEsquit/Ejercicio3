8d90b341b5974b87e8fad1cb98bcb810311643f4
AngelEsquit <esq23221@uvg.edu.gt>
2023-09-03 09:25:34 -0600

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,3 +1,10 @@
 public class Estudiante {
     
+    private String nombre;
+    private String apellido;
+    private int codigoUnico;
+    private String fecha;
+    private String email;
+    private ArrayList<Examen> examenes;
+
 }
