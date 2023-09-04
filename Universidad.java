e8ecf761e4b23548ada4a10576f5ef45abed0c38
AngelEsquit <esq23221@uvg.edu.gt>
2023-09-04 08:22:59 -0600

    Update Universidad.java

--- Universidad.java
+++ Universidad.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 public class Universidad {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Sede sede1 = new Sede();
-        Sede sede2 = new Sede();
-        Sede sede3 = new Sede();
+        ArrayList<Estudiante> estudiantes1 = new ArrayList<Estudiante>();
+        ArrayList<Estudiante> estudiantes2 = new ArrayList<Estudiante>();
+        ArrayList<Estudiante> estudiantes3 = new ArrayList<Estudiante>();
+        Sede sede1 = new Sede("Altiplano", estudiantes1);
+        Sede sede2 = new Sede("Central", estudiantes2);
+        Sede sede3 = new Sede("Occidente", estudiantes3);
 
         sede1.setNombre("Altiplano");
         sede2.setNombre("Central");
@@ -31,22 +34,45 @@ public class Universidad {
                 nombre = scanner.nextLine();
                 printMensaje("Ingrese su apellido");
                 apellido = scanner.nextLine();
-                printMensaje("Ingrese su fecha de nacimiento");
-                codigoUnico = scanner.nextInt();
                 printMensaje("Ingrese su carné");
+                codigoUnico = scanner.nextInt();
+                scanner.nextLine();
+                printMensaje("Ingrese su fecha de nacimiento");
                 fecha = scanner.nextLine();
                 printMensaje("Ingrese su email");
                 email = scanner.nextLine();
                 printMensaje("Ingrese su sede \n 1: Altiplano \n 2: Central \n 3: Occidente");
                 sede = scanner.nextInt();
+
                 switch (sede) {
                     case 1:
                         sede1.setEstudiantes(new Estudiante(nombre, apellido, codigoUnico, fecha, email));
+                        break;
                     case 2:
                         sede2.setEstudiantes(new Estudiante(nombre, apellido, codigoUnico, fecha, email));
+                        break;
                     case 3:
                         sede3.setEstudiantes(new Estudiante(nombre, apellido, codigoUnico, fecha, email));
+                        break;
                 }
+            }
+
+            else if (opcion == 2) {
+                printMensaje("Ingrese su sede \n 1: Altiplano \n 2: Central \n 3: Occidente");
+                sede = scanner.nextInt();
+
+                switch (sede) {
+                    case 1:
+                        printLista("Altiplano", sede1);
+                        break;
+                    case 2:
+                        printLista("Central", sede2);
+                        break;
+                    case 3:
+                        printLista("Occidente", sede3);
+                        break;
+                }
+                
             }
         }
     }
@@ -64,6 +90,15 @@ public class Universidad {
         System.out.println("");
         System.out.println("");
         System.out.println(mensaje);
+        System.out.println("");
+    }
+
+    public static void printLista(String sede, Sede sede10) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Lista de estudiantes de la sede " + sede);
+        sede10.getEstudiantes();
+        System.out.println();
         System.out.println("");
     }
 }
