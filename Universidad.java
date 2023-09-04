0a92f642d0655e927bed5e33cdd3100b3fd13f59
AngelEsquit <esq23221@uvg.edu.gt>
2023-09-04 07:47:55 -0600

    Update Universidad.java

--- Universidad.java
+++ Universidad.java
@@ -8,17 +8,62 @@ public class Universidad {
         Sede sede2 = new Sede();
         Sede sede3 = new Sede();
 
+        sede1.setNombre("Altiplano");
+        sede2.setNombre("Central");
+        sede3.setNombre("Occidente");
+
+        String nombre;
+        String apellido;
+        int codigoUnico;
+        String fecha;
+        String email;
+        int sede;
+
         boolean go = true;
 
         while (go == true) {
-            System.out.println("");
-            System.out.println("**************************************");
-            System.out.println("           Universidad 123");
-            System.out.println("**************************************");
-            System.out.println("Ingrese la opción que desee. \n 1: Ingresar datos \n 2: Estadísticas \n 3: Salir");
-            System.out.println("");
+            printMenu();
             int opcion = scanner.nextInt();
             scanner.nextLine();
+
+            if (opcion == 1) {
+                printMensaje("Ingrese su nombre");
+                nombre = scanner.nextLine();
+                printMensaje("Ingrese su apellido");
+                apellido = scanner.nextLine();
+                printMensaje("Ingrese su fecha de nacimiento");
+                codigoUnico = scanner.nextInt();
+                printMensaje("Ingrese su carné");
+                fecha = scanner.nextLine();
+                printMensaje("Ingrese su email");
+                email = scanner.nextLine();
+                printMensaje("Ingrese su sede \n 1: Altiplano \n 2: Central \n 3: Occidente");
+                sede = scanner.nextInt();
+                switch (sede) {
+                    case 1:
+                        sede1.setEstudiantes(new Estudiante(nombre, apellido, codigoUnico, fecha, email));
+                    case 2:
+                        sede2.setEstudiantes(new Estudiante(nombre, apellido, codigoUnico, fecha, email));
+                    case 3:
+                        sede3.setEstudiantes(new Estudiante(nombre, apellido, codigoUnico, fecha, email));
+                }
+            }
         }
+    }
+
+    public static void printMenu() {
+        System.out.println("");
+        System.out.println("**************************************");
+        System.out.println("           Universidad 123");
+        System.out.println("**************************************");
+        System.out.println("Ingrese la opción que desee. \n 1: Ingresar datos del estudiante \n 2: Ingresar notas \n 3: Estadísticas \n 4: Salir");
+        System.out.println("");
+    }
+
+    public static void printMensaje(String mensaje) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println(mensaje);
+        System.out.println("");
     }
 }
